c65c6f9fea7178d435bd09112f9de8bab69a232e
silviolimeira <sicemal@gmail.com>
2023-04-26 16:49:30 -0300

    More component work & Styling with CSS classes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import "./index.css";
+
 function App() {
-  return <div>Hello World! React</div>;
+  return (
+    <>
+      <h1>My Todos</h1>
+      <div className="card">
+        <h2>TITLE</h2>
+        <div className="actions">
+          <button className="btn">Delete</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
