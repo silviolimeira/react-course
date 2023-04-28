5199a39f6c31b0fa68c0ab69d461b7e5e9c64595
silviolimeira <sicemal@gmail.com>
2023-04-28 09:55:01 -0300

    Preparing The App For Http Request & Adding a Backend

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,14 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    console.log("addMeetupHandler");
+    // TODO: Send http request
+  }
   return (
     <>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </>
   );
 }
