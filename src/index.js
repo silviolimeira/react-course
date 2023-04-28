e48a5e3900c7fc23d27ee2e2794c780c7b7485d5
silviolimeira <sicemal@gmail.com>
2023-04-28 20:11:20 -0300

    Using Context in Components

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
+import { FavoritesContextProvider } from "./store/favorites-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <FavoritesContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FavoritesContextProvider>
   </React.StrictMode>
 );
